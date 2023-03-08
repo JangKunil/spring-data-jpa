747bc04169800859dffda339eb9429fa7a091067
JangKunil <jangkeya@gmail.com>
2023-03-08 19:42:17 +0900

    add Study class

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/Study.java
@@ -0,0 +1,28 @@
+package com.googlecode.hotire.springdatajpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Study {
+
+  @Id @GeneratedValue
+  private Long id;
+
+  private String name;
+
+  @ManyToOne
+  private Account owner;
+
+  public static Study createInstance(String name){
+    Study study = new Study();
+    study.setName(name);
+    return study;
+  }
+}
