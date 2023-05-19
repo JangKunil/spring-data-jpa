7ee54048957dea6e11e8d04f582feb4faeb1dd10
JangKunil <jangkeya@gmail.com>
2023-05-19 19:07:24 +0900

    add BookRepository

---
+++ src/test/java/com/googlecode/hotire/springdatajpa/event/BookRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.googlecode.hotire.springdatajpa.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+@Import(EntityListener.class)
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository repository;
+
+    @Test
+    void find() {
+        // given
+        final Book book = new Book();
+
+        // when
+        repository.save(book.publish());
+
+        // then
+    }
+}
