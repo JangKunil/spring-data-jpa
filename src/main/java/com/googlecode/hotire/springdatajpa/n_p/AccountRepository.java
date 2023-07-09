f059cac356e016f4974800ffcfdc8557ea6b16f9
JangKunil <jangkeya@gmail.com>
2023-07-09 11:26:04 +0900

    add AccountRepository
    Signed-off-by: JangKunil <jangkeya@gmail.com>

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/n_p/AccountRepository.java
@@ -0,0 +1,35 @@
+package com.googlecode.hotire.springdatajpa.n_p;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import com.googlecode.hotire.springdatajpa.Account;
+import com.googlecode.hotire.springdatajpa.dsl.AccountRepositoryCustom;
+
+/**
+ * N +1
+ */
+public interface AccountRepository extends JpaRepository<Account, Long>, AccountRepositoryCustom,
+                                           QuerydslPredicateExecutor {
+
+  /**
+   * Fetch Join
+   */
+  @Query("select a from Account a join fetch a.studies")
+  Set<Account> findAllJoinFetch();
+
+  @Query("select a from Account a left outer join Study s on a.id = s.owner.id")
+  List<Account> findAllJoinLeft();
+
+  /**
+   * EntityGraph
+   */
+  @EntityGraph(attributePaths = "studies")
+  @Query("select a from Account a")
+  List<Account> findAllEntityGraph();
+}
