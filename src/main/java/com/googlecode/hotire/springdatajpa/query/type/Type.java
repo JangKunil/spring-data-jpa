f4379822db2ff43350442159f33b37b5bf006ba9
JangKunil <jangkeya@gmail.com>
2023-07-11 00:01:32 +0900

    add TypeRepository
    Signed-off-by: JangKunil <jangkeya@gmail.com>

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/query/type/Type.java
@@ -0,0 +1,23 @@
+package com.googlecode.hotire.springdatajpa.query.type;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Type {
+    @Id @GeneratedValue
+    private Long id;
+    @Embedded
+    private Owner owner;
+}
