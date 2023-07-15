53c5ebc06b16b55b78a4f88e3c5e8d67b2628999
JangKunil <jangkeya@gmail.com>
2023-07-15 11:01:16 +0900

    import Inheritance
    Signed-off-by: JangKunil <jangkeya@gmail.com>

--- src/main/java/com/googlecode/hotire/springdatajpa/event/Item.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/event/Item.java
@@ -1,26 +1,22 @@
 package com.googlecode.hotire.springdatajpa.event;
 
-import org.springframework.data.domain.AbstractAggregateRoot;
-
-import javax.persistence.*;
-import java.util.function.Consumer;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class Item extends AbstractAggregateRoot<Item> {
+public abstract class Item extends AggregateEventPublisher<Item> {
 
     @GeneratedValue
     @Id
     private Long id;
 
-    public Item publish() {
-        registerEvent(new ItemEvent(this));
-        return this;
-    }
-
-    public Item publish(final Consumer<Item> consumer) {
-        registerEvent(new ItemEvent(this, consumer));
-        return this;
+    @Override
+    public EventCallbackAware<Item> getEvent() {
+        return new ItemEvent(this);
     }
 }
 
