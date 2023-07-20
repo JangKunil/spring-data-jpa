5efb07838c058de7c72f86fa4f84e0868cdfc845
JangKunil <jangkeya@gmail.com>
2023-07-20 23:45:12 +0900

    add Application
    Signed-off-by: JangKunil <jangkeya@gmail.com>

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/Application.java
@@ -0,0 +1,26 @@
+package com.googlecode.hotire.springdatajpa;
+
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+
+@SpringBootApplication
+public class Application implements InitializingBean {
+
+  @Autowired @Qualifier("coreRepository")
+  private JpaRepositoryFactoryBean factoryBean;
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+}
