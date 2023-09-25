c64b977df202bea53473eeb8854cf3514abc25eb
JangKunil <jangkeya@gmail.com>
2023-09-25 22:23:34 +0900

    Add auditorAwareRef to EnableJpaAuditing
    Signed-off-by: JangKunil <jangkeya@gmail.com>

--- src/main/java/com/googlecode/hotire/springdatajpa/Application.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/Application.java
@@ -1,5 +1,7 @@
 package com.googlecode.hotire.springdatajpa;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.InitializingBean;
@@ -7,8 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 
+@EnableJpaAuditing(
+        auditorAwareRef = "auditorAware",
+        dateTimeProviderRef = "dateTimeProvider"
+)
 @SpringBootApplication
 public class Application implements InitializingBean {
 
@@ -22,5 +32,15 @@ public class Application implements InitializingBean {
   @Override
   public void afterPropertiesSet() throws Exception {
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+  }
+
+  @Bean
+  public DateTimeProvider dateTimeProvider() {
+    return () -> Optional.of(OffsetDateTime.now());
+  }
+
+  @Bean
+  public AuditorAware auditorAware() {
+    return () -> Optional.of("SYSTEM");
   }
 }
