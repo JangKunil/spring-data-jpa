a17438828c52ce4f5d6c923ca5c61a10839bba2a
JangKunil <jangkeya@gmail.com>
2023-10-03 15:27:18 +0900

    add FoodTest
    Signed-off-by: JangKunil <jangkeya@gmail.com>

---
+++ src/test/java/com/googlecode/hotire/springdatajpa/audit/FoodTest.java
@@ -0,0 +1,33 @@
+package com.googlecode.hotire.springdatajpa.audit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class FoodTest {
+
+    @Test
+    void isAuditable() {
+        // given
+        final Food food = new Food();
+
+        // when
+        final boolean result = food.isAuditable();
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void getName() {
+        // given
+        final String name = "hello";
+        final Food food = new Food().setName(name);
+
+        // when
+        final String result = food.getName();
+
+        // then
+        assertThat(result).isEqualTo(name);
+    }
+}
