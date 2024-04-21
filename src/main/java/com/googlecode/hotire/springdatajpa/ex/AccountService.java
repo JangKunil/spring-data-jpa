e7b21fa6f95768c26c04edf854e51c376932bca0
JangKunil <jangkeya@gmail.com>
2024-04-21 15:34:46 +0900

    add AccountService

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/ex/AccountService.java
@@ -0,0 +1,54 @@
+package com.googlecode.hotire.springdatajpa.ex;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.googlecode.hotire.springdatajpa.n_p.AccountRepository;
+
+/**
+ * EntityManager Life Cycle Test
+ *
+ */
+//@Transactional
+@Service
+public class AccountService {
+  @Autowired
+  private AccountRepository accountRepository;
+//  @PersistenceContext(type = EXTENDED)
+  @PersistenceContext()
+  private EntityManager entityManager;
+
+  @Transactional
+  public void test() {
+    System.out.println(entityManager);
+    Account account = entityManager.find(Account.class, 1L);
+    account = entityManager.find(Account.class, 1L);
+    account = entityManager.find(Account.class, 1L);
+
+//    if (1==1)throw new RuntimeException("");
+
+    System.out.println("");
+
+//    entityManager.close();
+  }
+
+  public Account save(final Account account) {
+    return accountRepository.save(account);
+  }
+
+  public List<Account> list() {
+    return accountRepository.findAll();
+  }
+
+  @Transactional
+  public void service(final Consumer<Account> accountConsumer, final Account account) {
+    accountConsumer.accept(account);
+  }
+}
