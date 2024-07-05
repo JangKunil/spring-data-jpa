8a744db2988f8467ce54933f1ff1956164b1cd08
JangKunil <jangkeya@gmail.com>
2024-07-05 23:51:53 +0900

    add CustomLoader

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/persist/CustomLoader.java
@@ -0,0 +1,8 @@
+package com.googlecode.hotire.springdatajpa.core.persist;
+
+/**
+ *  @see org.hibernate.loader.entity.AbstractEntityLoader
+ *  @see org.hibernate.loader.Loader
+ */
+public class CustomLoader {
+}
