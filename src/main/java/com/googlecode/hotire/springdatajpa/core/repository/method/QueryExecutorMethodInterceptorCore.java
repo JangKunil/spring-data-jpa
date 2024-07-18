1383dee635c70f4eda912e714097ba77426c22cd
JangKunil <jangkeya@gmail.com>
2024-07-18 20:59:33 +0900

    add QueryExecutorMethodInterceptorCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryExecutorMethodInterceptorCore.java
@@ -0,0 +1,7 @@
+package com.googlecode.hotire.springdatajpa.core.repository.method;
+
+/**
+ * @see org.springframework.data.repository.core.support.RepositoryFactorySupport.QueryExecutorMethodInterceptor
+ */
+public class QueryExecutorMethodInterceptorCore {
+}
