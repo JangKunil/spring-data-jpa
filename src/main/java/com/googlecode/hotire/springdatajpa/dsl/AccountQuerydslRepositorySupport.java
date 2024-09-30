6266fcf98e4aec2f47cd2de74922e44186c16760
JangKunil <jangkeya@gmail.com>
2024-09-30 21:19:54 +0900

    add DefaultRepositoryConfigurationCore

--- src/main/java/com/googlecode/hotire/springdatajpa/dsl/AccountQuerydslRepositorySupport.java
+++ src/main/java/com/googlecode/hotire/springdatajpa/dsl/AccountQuerydslRepositorySupport.java
@@ -1,14 +1,12 @@
 package com.googlecode.hotire.springdatajpa.dsl;
 
-import static com.googlecode.hotire.springdatajpa.QAccount.account;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
-import com.googlecode.hotire.springdatajpa.Account;
-import com.googlecode.hotire.springdatajpa.QAccount;
+import com.googlecode.hotire.springdatajpa.ex.Account;
+import com.googlecode.hotire.springdatajpa.ex.QAccount;
 
 @Repository
 public class AccountQuerydslRepositorySupport extends QuerydslRepositorySupport {
@@ -17,8 +15,8 @@ public class AccountQuerydslRepositorySupport extends QuerydslRepositorySupport 
     super(Account.class);
   }
 
-  public List<Account> findByName(String name) {
-    return from(QAccount.account).where(account.username.eq(name)).fetch();
+  public List<Account> findByName(final String name) {
+    return from(QAccount.account).where(QAccount.account.username.eq(name)).fetch();
   }
 }
 
