d0c4c76386c5d1040ddcc1c94f4966d7296ce056
JangKunil <jangkeya@gmail.com>
2024-10-24 00:16:49 +0900

    add QueryAnnotationCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/repository/method/QueryAnnotationCore.java
@@ -0,0 +1,10 @@
+package com.googlecode.hotire.springdatajpa.core.repository.method;
+
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * @see Query
+ */
+public class QueryAnnotationCore {
+
+}
