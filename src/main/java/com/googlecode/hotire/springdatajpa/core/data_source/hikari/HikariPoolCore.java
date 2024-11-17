a8dff30a5914279b784d11338752e7cc4d6c16e6
JangKunil <jangkeya@gmail.com>
2024-11-17 23:46:01 +0900

    add HikariPoolCore

---
+++ src/main/java/com/googlecode/hotire/springdatajpa/core/data_source/hikari/HikariPoolCore.java
@@ -0,0 +1,17 @@
+package com.googlecode.hotire.springdatajpa.core.data_source.hikari;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.zaxxer.hikari.pool.HikariPool;
+
+/**
+ * @see HikariPool
+ */
+public interface HikariPoolCore {
+
+    /**
+     * @see HikariPool#getConnection() 
+     * @see HikariPool#getConnection(long)
+     */
+    Connection getConnection() throws SQLException;
+}
